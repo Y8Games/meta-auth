82537a39a6e4b89a046a9016bb765d8179284644
Eddie J <eddie@onefoldmedia.com>
2019-01-20 21:25:45 -0700

    Update index.js for socket.io support

--- index.js
+++ index.js
@@ -11,48 +11,20 @@ let cache = new NodeCache({
 
 class MetaAuth {
   constructor(options) {
-    return (req, res, next) => {
-      const DEFAULT_OPTIONS = {
-        signature: 'MetaSignature',
-        message: 'MetaMessage',
-        address: 'MetaAddress',
-        banner: '*** WARNING *** Ask the site to change the default banner *** WARNING ***'
-      }
-
-      this.options = Object.assign(
-        DEFAULT_OPTIONS,
-        options
-      )
-
-      // Address param is passed & isValidAddress
-      if (req.params[this.options.address]) {
-        const address = req.params[this.options.address];
-
-        if (ethUtil.isValidAddress(address)) {
-          const challenge = this.createChallenge(address);
-          let json = {
-            challenge
-          }
-          req.metaAuth = json;
-        }
-      }
-
-      // Challenge message returned with signature
-      if (req.params[this.options.message] &&
-        req.params[this.options.signature]) {
-
-        const recovered = this.checkChallenge(
-          req.params[this.options.message],
-          req.params[this.options.signature]
-        )
-        let token = {
-          recovered
-        }
-        req.metaAuth = token;
-      }
-
-      next();
+    const DEFAULT_OPTIONS = {
+      signature: 'MetaSignature',
+      message: 'MetaMessage',
+      address: 'MetaAddress',
+      banner: '*** WARNING *** Ask the site to change the default banner *** WARNING ***'
     }
+
+    this.options = Object.assign(
+      DEFAULT_OPTIONS,
+      options
+    )
+
+    this.createChallenge = this.createChallenge.bind(this);
+    this.checkChallenge = this.checkChallenge.bind(this);
   }
 
   createChallenge(address) {
@@ -103,4 +75,3 @@ class MetaAuth {
 
 
 module.exports = MetaAuth;
-
